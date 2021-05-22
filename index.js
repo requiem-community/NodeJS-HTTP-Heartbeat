aeaa76e3be180b4eb4005929ab5e7ed2d1de68f5
0xJacoby <jacoby34.dev@gmail.com>
2021-05-22 23:25:53 +0200

    code push

--- index.js
+++ index.js
@@ -3,17 +3,34 @@
 // deps
 const fetch = require("node-fetch");
 
+// constants
+const defaultDelay = 60000; // 1 minute
+
 class HTTPHeartbeat {
+    #interval;
+    #logging;
+    #url;
+
     constructor(url, logging = false) {
-        this.url = url;
-        this.logging = logging;
+        this.#url = url;
+        this.#logging = logging;
+    }
+
+    /* public props */
+    get url() {
+        return this.#url;
+    }
+
+    get interval() {
+        return (this.#interval) ? true : false;
     }
 
     /* Sends a GET request to the specified url */
-    ping = async () => {
+    ping = async (logging = false) => {
+        console.log(this.#interval);
         await fetch(this.url)
-            .then((res) => {
-                if (this.logging) this.logInfo(`INFO: Pinged status monitor`);
+            .then(() => {
+                if (logging) this.logInfo("Pinged status monitor");
             })
             .catch((err) => {
                 this.logInfo(`ERROR: "${err}"`);
@@ -23,17 +40,17 @@ class HTTPHeartbeat {
     /* Logs information to the console, formatted with the current UTC time */
     logInfo(infoText) {
         let d = new Date();
-        let timeStr = `${('0' + d.getUTCHours()).slice(-2)}:${('0' + d.getUTCMinutes()).slice(-2)}`;
-        console.log(`[UTC ${timeStr}] ${infoText}`);
+        let timeStr = `${("0" + d.getUTCHours()).slice(-2)}:${("0" + d.getUTCMinutes()).slice(-2)}`;
+        console.log(`[UTC ${timeStr}]: ${infoText}`);
     }
 
     /* Calls the ping method every specified delay (defaulted to 1 min) */
-    startInterval(delay = 60000) {
-        if (!this.interval) {
+    startInterval(delay = defaultDelay) {
+        if (!this.#interval) {
             this.ping();
-            this.interval = setInterval(this.ping, delay);
+            this.logInfo("The heartbeat function started");
 
-            this.logInfo("INFO: The heartbeat function started");
+            this.#interval = setInterval(this.ping, delay, this.#logging);
         } else {
             this.logInfo("ERROR: A heartbeat function is already running");
         }
@@ -41,8 +58,8 @@ class HTTPHeartbeat {
 
     /* Stops the heartbeat interval */
     stopInterval() {
-        clearInterval(this.interval);
-        this.interval = null;
+        clearInterval(this.#interval);
+        this.#interval = null;
     }
 }
 
